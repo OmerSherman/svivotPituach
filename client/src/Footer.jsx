92f361ae77b3f0e2c5ac39ac9be10e145a4ebf46
OmerSherman <omersherman22@gmail.com>
2026-05-26 20:29:16 +0300

    added Footer, Navbar, Layout

---
+++ client/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer({ year }) {
+    return (
+        <footer>
+            <p>React Course App</p>
+            <p>Built for learning components, routing, state, and API communication</p>
+        </footer>
+    );
+}
+
+export default Footer
